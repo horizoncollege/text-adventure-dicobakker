0e7d292fc21915d6e08ee7e736e62f460ba8ee0f
Niels Blom <123635308+pizzaineenpotje@users.noreply.github.com>
2024-03-19 09:47:55 +0100

    push classes
    test

---
+++ game.cs
@@ -0,0 +1,22 @@
+class game {
+    public static void Main() {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+class map {
+    public static void generate_map() {
+        //read from file map generator
+    }
+}
+
+class player {
+   
+}
+class inventory {
+    
+}
+
+class items {
+    
+}
